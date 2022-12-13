ed96505f7a61bfb055c4f22ab33537b9d9693fb1
Cameron White <cameron.jwhite91@gmail.com>
2022-12-12 20:57:40 -0500

    Building out class structure

--- xSock/xserver.h
+++ xSock/xserver.h
@@ -1,20 +1,34 @@
 #pragma once
 
+#include <deque>
+
 #include "asio.hpp"
+#include "message.h"
+#include "serv_con.h"
 #include "User.h"
 
 class XServer
 {
-	// Define user class
+public:
+	XServer();
+	Message get_next_msg();
+	int send_msg(const User& t_user);
 
-	// Define connection class
-
-	// Define method for handling requests
-
-	// Server should run in the background and allow the user to pull messages at will
-		// Stack messages in a deque and provide a function to retrieve the next message
-		// Connection needs to be asynchronus - have a process_message callback that interprets the message and loads it into the deque (or maybe just a que?)
+private:
+	std::deque<User> user_que;
+	std::deque<Message> msg_que;
 
 
 };
 
+// Define user class
+
+// Define connection class
+
+// Define message class
+
+// Define method for handling requests
+
+// Server should run in the background and allow the user to pull messages at will
+	// Stack messages in a deque and provide a function to retrieve the next message
+	// Connection needs to be asynchronus - have a process_message callback that interprets the message and loads it into the deque (or maybe just a que?)
